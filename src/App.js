e6178888a4d518c54095e110582c565f1afd91ac
Amit Kumar <4141amitkumar@gmail.com>
2025-04-05 15:33:00 +0530

    Improved

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import NavBar from './components/NavBar'; // ✅ Make sure this path is correct
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
-      {/* NavBar should go outside Routes */}
+      {}
       <NavBar />
 
-      {/* Define all Routes here */}
+      {}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
